02989bd3f2d2d482058db99f6775465899ec99ad
Robert Grant <rhgrant10@gmail.com>
2014-08-10 13:00:02 -0400

    Removed comment from config

--- groupy/config.py
+++ groupy/config.py
@@ -1,3 +1,3 @@
 API_URL = 'https://api.groupme.com/v3'
-IMAGE_API_URL = 'https://image.groupme.com'	# check this, might be wrong!
+IMAGE_API_URL = 'https://image.groupme.com'
 KEY_LOCATION = '~/.groupy.key'
