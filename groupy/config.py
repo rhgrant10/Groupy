d59a72970d7831353b8523cf4afbc6866480e747
Robert Grant <rhgrant10@gmail.com>
2014-08-11 20:10:34 -0400

    Commented config.py

--- groupy/config.py
+++ groupy/config.py
@@ -1,3 +1,11 @@
+# Set this to the base URL for the GroupMe API
+# (default is 'https://api.groupme.com/v3').
 API_URL = 'https://api.groupme.com/v3'
+
+# Set this to the base URL for the GroupMe image service 
+# (default is 'https://image.groupme.com').
 IMAGE_API_URL = 'https://image.groupme.com'
+
+# Set this to the location of the file containing your 
+# API access token (API key).
 KEY_LOCATION = '~/.groupy.key'
