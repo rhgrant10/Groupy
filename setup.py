ba54fa051c990717306d9e155c6ba4a80a65f306
Robert Grant <rhgrant10@gmail.com>
2014-08-13 11:12:52 -0400

    Preparing for PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'Groupy',
+    packages = ['Groupy'], # this must be the same as the name above
+    version = '0.3.0',
+    description = 'The simple yet powerful wrapper for the GroupMe API',
+    author = 'Robert Grant',
+    author_email = 'rhgrant10@gmail.com',
+    url = 'https://github.com/rhgrant10/Groupy', # use the URL to the github repo
+    download_url = 'https://github.com/rhgrant10/Groupy/tarball/0.3.0', # I'll explain this in a second
+    keywords = ['api', 'GroupMe'], # arbitrary keywords
+    classifiers = [],
+)
