a484d6e717e1ef63389f480a2aaa545c901164b1
Robert Grant <rhgrant10@gmail.com>
2014-12-08 22:56:48 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from distutils.core import setup
+
 setup(
     name = 'GroupyAPI',
     packages = ['groupy', 'groupy.object', 'groupy.api'],
-    version = '0.5.6',
-    install_requires = [
-        'responses==0.2.2',
-        'requests==2.3.0',
-        'Pillow==2.5.3'
-    ],
+    version = '0.5.7',
+    install_requires = open('requirements.txt').read().splitlines(),
     description = 'The simple yet powerful wrapper for the GroupMe API',
     author = 'Robert Grant',
     author_email = 'rhgrant10@gmail.com',
