a25611de532ea7f346e0d6c7ea2b4a2427b4c4da
Robert Grant <rhgrant10@gmail.com>
2017-12-03 02:58:48 -0500

    Add a util for parsing share urls

--- groupy/utils.py
+++ groupy/utils.py
@@ -11,3 +11,12 @@ def urljoin(base, path=None):
             base += '/'
         url = urllib.parse.urljoin(base, str(path))
     return url
+
+
+def parse_share_url(share_url):
+    """Return the group_id and share_token in a group's share url.
+
+    :param str share_url: the share url of a group
+    """
+    *__, group_id, share_token = share_url.rstrip('/').split('/')
+    return group_id, share_token
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -16,3 +16,20 @@ class UrlJoinTests(unittest.TestCase):
 
 class TrailingSlashUrlJoinTests(UrlJoinTests):
     url = 'http://example.com/foo/'
+
+
+class ParseShareUrlTests(unittest.TestCase):
+    url = 'http://example.com/foo/group_id/share_token'
+
+    def setUp(self):
+        self.group_id, self.share_token = utils.parse_share_url(self.url)
+
+    def test_group_id_is_correct(self):
+        self.assertEqual(self.group_id, 'group_id')
+
+    def test_share_token_is_correct(self):
+        self.assertEqual(self.share_token, 'share_token')
+
+
+class TrailingSlashParseShareUrlTests(ParseShareUrlTests):
+    url = 'http://example.com/foo/group_id/share_token/'
