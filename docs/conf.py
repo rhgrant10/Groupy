e61b17e328676f810c379e737d50a1209a73be1f
Robert Grant <rhgrant10@gmail.com>
2017-12-15 10:41:42 -0500

    Update the copyright date

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Groupy'
-copyright = '2014, Robert Grant'
+copyright = '2018, Robert Grant'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
